6668818d913294819ee5b3ed72b9776c349c617b
Rosuelo996 <rosuelo996.sr@gmail.com>
2025-12-23 23:17:27 +0000

    practice

---
+++ user.js
@@ -0,0 +1,30 @@
+const postListEl = document.querySelector(".post-list");
+const id = localStorage.getItem("id");
+
+async function onSearchChange(event) {
+  const id = event.target.value;
+  renderPosts(id)
+}
+
+async function renderPosts(userId) {
+  const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+  const postsData = await posts.json();
+  console.log(postsData);
+  postListEl.innerHTML = postsData
+    .map((post) => post.HTML(post)).join("");
+}
+
+function postHTML(post) {
+    `
+    <div class="post">
+      <div class="post__title">
+        ${post.title}
+      </div>
+      <p class="post__body">
+        ${post.body}
+      </p>
+    </div>
+    `
+}
+
+renderPosts(id);
